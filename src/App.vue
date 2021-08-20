1dec140feec36d8d262fb1c78187e93c4930b78e
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-08-20 21:00:57 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-tabs/>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTabs from './components/AppTabs.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppTabs,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ background: rgb(114, 113, 113);
 }
 </style>
---
+++ src/components/AppTabs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tabs">
+   <ul class="tab-links">
+     <li v-for="tab in tabs" :class="{'active':tab.active}" @click="set Active(tab)">{{tab.label}}</li>
+   </ul>
+   <slot></slot>
+
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTabs',
+  data: function() {
+    return{
+      tabs:[]
+    };
+  },
+  created: function() {
+    this.tabs = this.$children;
+  },
+  methods: {
+    setActive: function(tab) {
+    this.tabs.forEach(function (t) {
+      t.active = t === tab;
+    });
+   }
+  }
+
+}
+</script>
+
+<style scoped>
+  .app-tabs {
+  background: rgb(114, 113, 113);
+  }
+</style>
